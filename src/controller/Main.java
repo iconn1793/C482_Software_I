751f4f2ce680c37003aa06c7cf52c1e482010536
Ian R Connelly <11connellyi@gmail.com>
2021-09-09 11:07:29 -0400

    Added Controller file stubs and refactored classes

--- src/controller/Main.java
+++ src/controller/Main.java
@@ -11,7 +11,7 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("/view/mainView.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("/view/MainMenu.fxml"));
 
         primaryStage.setTitle("Inventory Management System");
         primaryStage.setScene(new Scene(root, 1000, 500));
